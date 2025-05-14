957c79845c81f3de9020b61fbe240e71755d71e1
Manu-jothish <manujothish722@gmail.com>
2025-05-14 12:09:33 +0530

    add delete update

--- backend/controller/todoController.js
+++ backend/controller/todoController.js
@@ -1,36 +1,77 @@
 import Todos from "../models/todoModels.js";
 
-let creatTodos = async (req,res)=> {
+let creatTodos = async (req, res) => {
     console.log(req.body);
-    
-     try {
-        let todo= await Todos.create({
-            title:req.body.title,
-            description : req.body.description
+
+    try {
+        let todo = await Todos.create({
+            title: req.body.title,
+            description: req.body.description
         })
 
-       res.json(todo)
+        res.json(todo)
 
-     } catch (error) {
-        console.log(error);
-        
-     }
-
-}
-
-let getTodos =  async(req,res)=>{
-    try {
-        let todos= await Todos.find()
-        console.log(todos)
-        res.json(todos)
-        
     } catch (error) {
         console.log(error);
-        
+
     }
-    
+
 }
 
+let getTodos = async (req, res) => {
+    try {
+        let todos = await Todos.find()
+        console.log(todos)
+        res.json(todos)
+
+    } catch (error) {
+        console.log(error);
+
+    }
+
+}
+
+const deleteTodo = async (req, res) => {
+    console.log(req.params.id);
+
+    try {
+        const deleteTodo = await Todos.findByIdAndDelete(req.params.id)
+        if (!deleteTodo) {
+            return res.status(404).json({ message: 'Todo not found' })
+        }
+
+        return res.json({ message: 'deleted' })
+    } catch (error) {
+        console.log(error);
+
+    }
+
+}
+
+
+
+const updateTodo = async (req, res) => {
+    console.log(req.query.id,)
+
+    try {
+        let {title,description,isCompleted}=  req.body
+        let updateTodo = await Todos.findByIdAndUpdate(req.query.id, {title,description,isComplete})
+
+        if (!updateTodo) {
+            return res.status(404).json({ message: 'todo not found' })
+        }
+        return res.json({ message: 'Todo updated Successfully' })
+
+
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+
+
 export {
-    creatTodos,getTodos
+    creatTodos, getTodos, updateTodo, deleteTodo
 }
--- backend/routes/todoRout.js
+++ backend/routes/todoRout.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import Todos from '../models/todoModels.js';
-import { creatTodos,getTodos } from '../controller/todoController.js';
+import { creatTodos,getTodos ,updateTodo,deleteTodo} from '../controller/todoController.js';
 
 
 
@@ -10,5 +10,9 @@ const todoRouter = express.Router()
 todoRouter.post('/',creatTodos)
 
 todoRouter.get('/getTodos',getTodos)
+
+todoRouter.patch('/updatetodo', updateTodo)
+
+todoRouter.delete('/:id', deleteTodo)
 
 export default todoRouter
