b0e595332b947c07a105e147657684be0350e4f0
Manu-jothish <manujothish722@gmail.com>
2025-06-03 12:51:47 +0530

    completed todo slice

---
+++ frontend/src/slices/todoApiSlices.js
@@ -0,0 +1,17 @@
+
+import { apiSlice } from "./apiSlice";
+
+
+const todoApiSlice = apiSlice.injectEndpoints({
+    endpoints:(builder)=>({
+        getTodos:builder.query({
+            query:()=>({
+                url:'/api/todo/getTodos'
+            })
+        })
+    })
+})
+
+export const {
+    useGetTodosQuery
+} = todoApiSlice
